b5921d249fccde18bb86284a9e5baceac2090810
Itamar Ostricher <itamarost@gmail.com>
2017-11-16 19:27:50 +0200

    Demo implementation

---
+++ src/helpers/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: 'AIzaSyDOthcx9EAS4Hqy7YI0_vrNV923aM8x41w',
+  authDomain: 'weight-track-644d5.firebaseapp.com',
+  databaseURL: 'https://weight-track-644d5.firebaseio.com',
+  projectId: 'weight-track-644d5',
+  storageBucket: 'weight-track-644d5.appspot.com',
+  messagingSenderId: '102598797725'
+}
